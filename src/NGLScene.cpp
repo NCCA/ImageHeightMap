395219217cebdac4cdf8b759a160d983851d2ccf
jmacey <jmacey@bournemouth.ac.uk>
2017-09-25 09:28:55 +0100

    updated for new mat4 class

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -189,7 +189,7 @@ void NGLScene::paintGL()
   (*shader)["ColourShader"]->use();
 
   ngl::Mat4 MVP;
-  MVP=m_mouseGlobalTX*m_cam.getVPMatrix();
+  MVP=m_cam.getVPMatrix()*m_mouseGlobalTX;
 
   shader->setUniform("MVP",MVP);
 
